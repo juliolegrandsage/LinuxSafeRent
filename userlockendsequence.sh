12a54b0261c2375fcddbe2b77f3583119cb4e398
foreach <112372790+forEachLoopz@users.noreply.github.com>
2024-02-29 16:45:29 +0100

    V0.0.3

--- userlockendsequence.sh
+++ userlockendsequence.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Chemin du fichier contenant le mot de passe
-password_file="/var/lib/password.txt"
-
-# Lire le mot de passe depuis le fichier
-password=$(cat "$password_file")
-
+# Mot de passe en dur (remplacez "votre_mot_de_passe" par votre mot de passe réel)
+new_password="fraise"
 # Chemin vers le fichier de date d'expiration
-expiration_date_file="/var/lib/fin_acces_date.txt"
+expiration_date_file="fin_acces_date.txt"
 
 # bool pour l'état de blocage
 is_locked=false
@@ -16,10 +12,7 @@ is_locked=false
 restarting=false
 
 # Chemin vers le fichier de configuration Zenity
-zenity_config_file="/var/lib/zenity_config.txt"
-
-# Chemin vers le fichier de configuration de l'application de démarrage
-autostart_file="$HOME/.config/autostart/userlockendsequence.desktop"
+zenity_config_file="zenity_config.txt"
 
 # Messages Zenity par défaut
 default_expiration_message="Vous êtes dans la dernière période de 30 jours avant la date d'échéance.\n\nPensez à renouveler votre accès.\n\nContact : 06 22 09 86 53 / clech.michel@wanadoo.fr"
@@ -30,20 +23,31 @@ is_user_logged_in() {
     who | grep -q "$(whoami)"
 }
 
-
-
-# Fonction pour afficher une notification un mois avant la date d'échéance
 notify_one_month_before() {
     current_timestamp=$(date +"%s")
     expiration_datetime=$(cat "$expiration_date_file")
     expiration_timestamp=$(date -d "$expiration_datetime" +"%s")
-    one_month_before=$(date -d "$expiration_datetime -1 month" +"%s")
+    three_weeks_before=$(date -d "$expiration_datetime -1 month -3 weeks" +"%s")
 
-    # Si on est dans la dernière période de 30 jours avant échéance
-    if [ $current_timestamp -ge $one_month_before ] && [ $current_timestamp -lt $expiration_timestamp ]; then
-        # Si c'est un lundi à 16 heures
-        if [ "$(date +"%u %H" -d "$expiration_datetime -1 month")" = "1 16" ]; then
-            zenity --info --title="Information" --text="$(cat "$zenity_config_file")"
+    # Si on est dans le dernier mois avant échéance et qu'il reste plus de trois semaines
+    if [ $current_timestamp -ge $three_weeks_before ] && [ $current_timestamp -lt $expiration_timestamp ]; then
+        # Déterminer le nombre de secondes restantes (ici, par exemple, on utilise 10 secondes pour simuler des tests rapides)
+        remaining_seconds=$(($expiration_timestamp - $current_timestamp))
+
+        # Si le reste de la division par 3 est égal à 0 (c'est-à-dire toutes les trois secondes)
+        if [ $(($remaining_seconds % 3)) -eq 0 ]; then
+            # Afficher un message Zenity différent en fonction des secondes restantes
+            case $(($remaining_seconds / 3)) in
+                3)  # Trois secondes restantes
+                    zenity --info --title="Information" --text="Message pour les trois secondes restantes"
+                    ;;
+                2)  # Deux secondes restantes
+                    zenity --info --title="Information" --text="Message pour les deux secondes restantes"
+                    ;;
+                1)  # Une seconde restante
+                    zenity --info --title="Information" --text="Message pour la dernière seconde"
+                    ;;
+            esac
         fi
     fi
 }
@@ -65,27 +69,40 @@ while true; do
     current_timestamp=$(date +"%s")
     expiration_datetime=$(cat "$expiration_date_file")
     expiration_timestamp=$(date -d "$expiration_datetime" +"%s")
+        
+    sleep 5
+    zenity --info --title="Information" --text="Message pour les trois semaines restantes"
+    
+    sleep 5
+    zenity --info --title="Information" --text="Second message"
+    
+    sleep 5
+    zenity --info --title="Information" --text="Troisième message"
 
     # Si le temps restant est inférieur ou égal à zéro et le verrouillage n'a pas été effectué, exécuter le script de modification de mot de passe et redémarrage
     if [ $current_timestamp -ge $expiration_timestamp ] && [ "$is_locked" = false ] && [ "$restarting" = false ]; then
-
-        change_lockscreen_wallpaper
-        # Exécuter le script de modification de mot de passe et redémarrage
-
-        echo -e "$password\n$password" | sudo passwd $(whoami)
         is_locked=true  # Mettre à jour le statut du verrouillage
+        echo -e "fraise\nfraise" | sudo passwd $(whoami)  # Changer le mot de passe
+
         restarting=true  # Indiquer que le redémarrage est en cours
         zenity --info --title="Information" --text="$(cat "$zenity_config_file")"
-	sudo pkill -u $(whoami)
-        cinnamon-screensaver-command --lock
-        
+
+        if [ -n "$DISPLAY" ]; then
+
+            # Pour les environnements graphiques
+            pkill -u $(whoami)  # Déconnexion de l'utilisateur actuel
+            cinnamon-screensaver-command --lock || gnome-screensaver-command --lock || dm-tool lock
+        else
+            # Pour les environnements non graphiques
+            echo "Impossible de verrouiller l'écran dans cet environnement."
+        fi
     fi
 
     # Réinitialiser la variable is_locked et restarting à false si l'utilisateur est connecté
     if is_user_logged_in; then
         is_locked=false
         restarting=false
-        restore_default_wallpaper  # Restaurer le fond d'écran par défaut
+        # restore_default_wallpaper  # Restaurer le fond d'écran par défaut
     fi
 
     # Afficher une notification un mois avant la date d'échéance
@@ -96,4 +113,3 @@ while true; do
 
     sleep 60  # Attendre 60 secondes avant de vérifier à nouveau
 done
-
--- userlockpublicrelease.sh
+++ userlockpublicrelease.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Chemin vers le fichier de date d'expiration
-expiration_date_file="/var/lib/fin_acces_date.txt"
+expiration_date_file="fin_acces_date.txt"
 # Chemin vers le fichier contenant le mot de passe
-password_file="/var/lib/password.txt"
+password_file="password.txt"
 # Chemin vers le fichier contenant les messages Zenity
-zenity_config_file="/var/lib/zenity_config.txt"
+zenity_config_file="zenity_config.txt"
 # Chemin vers le fichier contenant les messages d'avertissement
-warning_config_file="/var/lib/warning_config.txt"
+warning_config_file="warning_config.txt"
 
 # Messages Zenity par défaut
 default_expiration_message="Vous êtes dans la dernière période de 30 jours avant la date d'échéance.\n\nPensez à renouveler votre accès.\n\nContact : 06 22 09 86 53 / clech.michel@wanadoo.fr"
@@ -18,56 +18,11 @@ default_warning_message="Attention ! Votre accès expire bientôt. Veuillez reno
 get_expiration_datetime() {
     # Sélection de la date
     selected_date=$(zenity --calendar --title="Choisir la date d'expiration" --text="Sélectionnez la date d'expiration de l'accès." --date-format="%Y-%m-%d" --window-icon="calendar" --width=300 --height=300)
-
-    # Sélection de l'heure
-    selected_time=$(zenity --entry --title="Choisir l'heure d'expiration" --text="Saisissez l'heure d'expiration (format 24 heures, par exemple, 14:30)" --entry-text "12:00")
-
+	
+		selected_time="00:00"
     echo "$selected_date $selected_time"
 }
 
-# Fonction pour configurer le mot de passe avec Zenity
-configure_password() {
-    # Saisie du nouveau mot de passe
-    new_password=$(zenity --password --title="Configuration du mot de passe" --text="Entrez le nouveau mot de passe")
-
-    # Vérification du nouveau mot de passe
-    verify_password=$(zenity --password --title="Configuration du mot de passe" --text="Veuillez confirmer le nouveau mot de passe")
-
-    # Comparaison des mots de passe
-    if [ "$new_password" != "$verify_password" ]; then
-        zenity --error --title="Erreur" --text="Les mots de passe ne correspondent pas. Veuillez réessayer."
-        configure_password  # Appel récursif en cas de non-correspondance
-    else
-        echo "$new_password" | sudo tee "$password_file" > /dev/null
-        zenity --info --title="Information" --text="$default_password_success_message"
-    fi
-}
-
-# Fonction pour configurer les messages Zenity
-configure_zenity_messages() {
-    # Vérifier si le fichier de configuration Zenity existe
-    if [ -e "$zenity_config_file" ]; then
-        # Charger les messages Zenity à partir du fichier
-        current_content=$(sudo cat "$zenity_config_file")
-    else
-        # Utiliser les messages Zenity par défaut
-        current_content="$default_expiration_message"
-    fi
-
-    # Afficher une boîte de dialogue avec une zone de texte contenant le contenu actuel
-    new_content=$(zenity --text-info --title="Édition des messages Zenity" --editable --width=400 --height=300 --text="$current_content")
-
-    # Enregistrer le nouveau contenu dans le fichier
-    echo "$new_content" | sudo tee "$zenity_config_file" > /dev/null
-}
-
-
-
-# Demander à l'utilisateur de configurer les messages Zenity
-configure_zenity_messages
-
-# Demander à l'utilisateur de configurer les messages d'avertissement
-configure_warning_messages
 
 # Demander à l'utilisateur de choisir la date et l'heure d'expiration
 expiration_datetime=$(get_expiration_datetime)
@@ -78,9 +33,10 @@ echo "$expiration_datetime" | sudo tee "$expiration_date_file" > /dev/null
 # Notification pour informer que la date a été enregistrée avec succès
 zenity --info --title="Information" --text="Moment d'échéance : $expiration_datetime"
 
-# Configurer le mot de passe
-configure_password
-zenity --info --title="Info" --text="Configuration terminée. Redemarrage dans 1 minute."
 
+# Informer que la configuration est terminée et que le redémarrage aura lieu dans 1 minute
+zenity --info --title="Info" --text="Configuration terminée. Redémarrage dans 1 minute."
+
+# Redémarrer le système dans 1 minute
 sudo shutdown -r +1
 
